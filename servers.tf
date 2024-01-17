5d5a3b4fd5cb07e6a435883f1b3389a83af84dd0
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-17 21:04:19 +0530

    roboshop - multi env data setup

--- servers.tf
+++ servers.tf
@@ -1,12 +1,3 @@
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
-
-data "aws_security_group" "selected" {
-  name = "practice-devops-allow-all"
-}
 
 resource "aws_instance" "instance" {
   for_each = var.components
