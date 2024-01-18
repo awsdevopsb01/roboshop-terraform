f6fedf2e9bca0849007fee56456cdcf5485ad069
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-18 21:41:23 +0530

    terraform provisioners

--- servers.tf
+++ servers.tf
@@ -10,6 +10,27 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance, aws_route53_record.dnsrecord]
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = [aws_instance.instance[each.value["name"]].private_ip]
+    }
+
+    inline = [
+      "rm -rf roboshop-adv-shell",
+      "git clone https://github.com/awsdevopsb01/roboshop-adv-shell.git",
+      "cd roboshop-adv-shell",
+      "sudo bash ${each.value["name"]}}.sh"
+    ]
+}
+}
+
+
 resource "aws_route53_record" "dnsrecord" {
   for_each = var.components
   zone_id = "Z01307132WU1DJMGVKGO6"
